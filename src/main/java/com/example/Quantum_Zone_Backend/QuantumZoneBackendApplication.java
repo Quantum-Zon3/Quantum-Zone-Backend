fc30f66b5b9baa3e4b128e800587ebc5e5742f54
Miguel Angel <lievano.miguel.3534@eam.edu.co>
2025-04-01 08:01:19 -0500

    Commit Inicial

---
+++ src/main/java/com/example/Quantum_Zone_Backend/QuantumZoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quantum_Zone_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumZoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumZoneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Quantum_Zone_Backend/QuantumZoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quantum_Zone_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumZoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
