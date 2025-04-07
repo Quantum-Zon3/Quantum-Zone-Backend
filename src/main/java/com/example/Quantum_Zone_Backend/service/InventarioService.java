0e8903d356f4e264a1e2361ad9ab05457f5ddbc9
CarlosLopez <lopez.carlos.4807@eam.edu.co>
2025-04-06 20:56:36 -0500

    Se modifico el services para solucion de errores pikos

--- src/main/java/com/example/Quantum_Zone_Backend/service/InventarioService.java
+++ src/main/java/com/example/Quantum_Zone_Backend/service/InventarioService.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.Quantum_Zone_Backend.modelo.*;
+import com.example.Quantum_Zone_Backend.repository.InventarioRepository;
 public class InventarioService {
 	@Autowired
 	private final InventarioRepository inventarioRepository;
@@ -31,37 +31,35 @@ public class InventarioService {
 		
 		// Guardar los puestos en el inventario
 		List<Puesto> listaDePuestos = puestoService.findAll();
+		Map<String, Puesto> puesto = new HashMap<>();
 		for (int i = 0; i < listaDePuestos.size() ; i++) {
-			Map<String, Puesto> puesto = new HashMap<>();
 			puesto.put(listaDePuestos.get(i).getId(), listaDePuestos.get(i));
 		}
 		inventario.setPuestos(puesto);
 		
 		// Guardar las consolas en el inventario
 		List<Consola> listaDeConsolas = consolaService.findAll();
+		Map<String, Consola> consola = new HashMap<>();
 		for (int i = 0; i < listaDeConsolas.size() ; i++) {
-			Map<String, Consola> consola = new HashMap<>();
 			consola.put(listaDeConsolas.get(i).getId(), listaDeConsolas.get(i));
 		}
 		inventario.setConsolas(consola);
 		// Guardar los objetos en el inventario
 		List<Objeto> listaDeObjetos = objetoService.findAll();
+		Map<String, Objeto> objeto = new HashMap<>();
 		for (int i = 0; i < listaDeObjetos.size() ; i++) {
-			Map<String, Objeto> objeto = new HashMap<>();
 			objeto.put(listaDeObjetos.get(i).getId(), listaDeObjetos.get(i));
 		}
 		inventario.setObjetos(objeto);
 		// Guardar los videojuegos en el inventario
 		List<VideoJuego> listaDeVideojuegos = videojuegoService.findAll();
+		Map<String, VideoJuego> videojuego = new HashMap<>();
 		for (int i = 0; i < listaDeVideojuegos.size() ; i++) {
-			Map<String, VideoJuego> videojuego = new HashMap<>();
 			videojuego.put(listaDeVideojuegos.get(i).getId(), listaDeVideojuegos.get(i));
 		}
-		inventario.setVideojuegos(videojuego);
+		inventario.setJuegos(videojuego);
 		// Guardar el inventario en la base de datos
 		save(inventario);
-		
-		
 	}
 	
 	public Inventario save(Inventario inventario) {
