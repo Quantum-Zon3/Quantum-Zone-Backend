c4a2c81823d179b621881dd9c7bc6ca1fbe7e73f
CarlosLopez <lopez.carlos.4807@eam.edu.co>
2025-04-10 17:14:55 -0500

    Se solucionaron errores de looping y nulls

--- src/main/java/com/example/Quantum_Zone_Backend/service/VideojuegoRentadoService.java
+++ src/main/java/com/example/Quantum_Zone_Backend/service/VideojuegoRentadoService.java
@@ -18,60 +18,64 @@ import com.example.Quantum_Zone_Backend.service.VideoJuegoService;
 
 @Service
 public class VideojuegoRentadoService {
-        private VideojuegosRentadosRepository videojuegoRRepository;
-        private VideojuegoRentadoService videojuegoRService;
-        private VideoJuegoService videojuegoService;
-        private ClienteService clienteService;
-        
-    
-        @Autowired
-	public VideojuegoRentadoService(VideojuegosRentadosRepository videojuegoRRepository ,VideojuegoRentadoService videojuegoRService, 
-                VideoJuegoService videojuegoService, ClienteService clienteService) {
-            
+	private VideojuegosRentadosRepository videojuegoRRepository;
+	private VideojuegoRentadoService videojuegoRService;
+	private VideoJuegoService videojuegoService;
+	private ClienteService clienteService;
+
+	@Autowired
+	public VideojuegoRentadoService(VideojuegosRentadosRepository videojuegoRRepository,
+			VideoJuegoService videojuegoService, ClienteService clienteService) {
+
 		this.videojuegoRRepository = videojuegoRRepository;
-		this.videojuegoRService = videojuegoRService;
-                this.videojuegoService = videojuegoService;
-                this.clienteService = clienteService;
+		this.videojuegoService = videojuegoService;
+		this.clienteService = clienteService;
 		// Inicializamos algunos datos
 		initSampleData();
 	}
+
 	private void initSampleData() {
 		List<Cliente> clientes = clienteService.findAll();
-                List<VideoJuego> videojuegos = videojuegoService.findAll();
-		for (int i = 0; i <clientes.size() ; i++) {
-                    for(int j=0; i<videojuegos.size(); j++){
-                    	
-						VideojuegoRentado videojuegoRentado = new VideojuegoRentado(clientes.get(0), videojuegos.get(0), LocalDate.now(), LocalDate.now().plusDays(2));
-						save(videojuegoRentado);
-                    }
+		List<VideoJuego> videojuegos = videojuegoService.findAll();
+		for (int i = 0; i < clientes.size(); i++) {
+			for (int j = 0; i < videojuegos.size(); j++) {
+
+				VideojuegoRentado videojuegoRentado = new VideojuegoRentado(clientes.get(0), videojuegos.get(0),
+						LocalDate.now(), LocalDate.now().plusDays(2));
+				save(videojuegoRentado);
+			}
 		}
-		
-		
+
 	}
-        
+
 	// encontrar un puesto por id
 	public VideojuegoRentado findById(String id) {
 		return videojuegoRRepository.findById(id);
 	}
+
 	// listar todos los puestos
 	public List<VideojuegoRentado> findAll() {
 		return videojuegoRRepository.findAll();
 	}
+
 	// eliminar un puesto por id
 	public void deleteById(String id) {
 		videojuegoRRepository.deleteById(id);
 	}
+
 	// actualizar un puesto
 	public VideojuegoRentado update(VideojuegoRentado videojuegoR) {
 		return videojuegoRRepository.update(videojuegoR);
 	}
+
 	// buscar puesto por filtros
-	public List<VideojuegoRentado> findByFilters(String id, Cliente cliente, VideoJuego videojuego, LocalDate fechaDeRenta, LocalDate fechaDeDevolucion) {
-		return videojuegoRRepository.findByFilters(id , fechaDeRenta, fechaDeDevolucion, cliente, videojuego);
+	public List<VideojuegoRentado> findByFilters(String id, Cliente cliente, VideoJuego videojuego,
+			LocalDate fechaDeRenta, LocalDate fechaDeDevolucion) {
+		return videojuegoRRepository.findByFilters(id, fechaDeRenta, fechaDeDevolucion, cliente, videojuego);
 	}
-		
-		public VideojuegoRentado save(VideojuegoRentado videojuegoR) {
+
+	public VideojuegoRentado save(VideojuegoRentado videojuegoR) {
 		return videojuegoRRepository.save(videojuegoR);
 	}
-    
+
 }
