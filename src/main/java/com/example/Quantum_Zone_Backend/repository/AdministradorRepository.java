ed11f991d2489f130bc686b30425b805b2bc4be3
Carlos Patino <carlosmariopatinoraamirez@gmail.com>
2025-05-13 00:50:13 -0500

    Actualizado administrador y cliente repositories para el manejo de base de datos

--- src/main/java/com/example/Quantum_Zone_Backend/repository/AdministradorRepository.java
+++ src/main/java/com/example/Quantum_Zone_Backend/repository/AdministradorRepository.java
@@ -1,52 +1,70 @@
 package com.example.Quantum_Zone_Backend.repository;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Optional;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 import com.example.Quantum_Zone_Backend.modelo.Administrador;
 
 @Repository
 public class AdministradorRepository {
-	private final Map<String, Administrador> baseDeDatos = new HashMap<>();
+	@PersistenceContext
+	private EntityManager entityManager;
 	//Guardar Administrador
+	@Transactional
 	public Administrador save(Administrador administrador) {
-		baseDeDatos.put(administrador.getId(), administrador);
+		entityManager.persist(administrador);
 		return administrador;
 	}
 	//Buscar todos los Administradores
+	@Transactional
 	public List<Administrador> findAll() {
-		return new ArrayList<>(baseDeDatos.values());
+		Query query = entityManager.createNativeQuery("SELECT * FROM Administrador ", Administrador.class);
+		return query.getResultList();
 	}
 	//Buscar administrador por id
-	public Administrador findById(String id) {
-		return baseDeDatos.get(id);
+	@Transactional
+	public Optional <Administrador> findById(String id) {
+		Query query = entityManager.createNativeQuery("SELECT * FROM Administrador WHERE id = :id", Administrador.class);
+		query.setParameter("id", id);
+		try {
+			Administrador administrador = (Administrador) query.getSingleResult();
+			return Optional.of(administrador);
+		} catch (Exception e) {
+			return Optional.empty();
+		}
 	}
 	//Eliminar administrador por id
-	public void deleteById(String id) {
-		baseDeDatos.remove(id);
+	@Transactional
+	public boolean deleteById(String id) {
+		Query query = entityManager.createNativeQuery("DELETE FROM Administrador WHERE id = :id");
+		query.setParameter("id", id);
+		int delete = query.executeUpdate();
+		return delete > 0;
 	}
 	//Actualizar administrador
-	public Administrador update(Administrador administrador) {
-		if (baseDeDatos.containsKey(administrador.getId())) {
-			baseDeDatos.put(administrador.getId(), administrador);
-
-			return administrador;
+	@Transactional
+	public Optional<Administrador> update(String id,Administrador administrador) {
+		Query query = entityManager.createNativeQuery("UPDATE Administrador SET nombre = :nombre, contraseña = :contraseña, edad = :edad, cedula = :cedula WHERE id = :id");
+		query.setParameter("nombre", administrador.getNombre());
+		query.setParameter("contraseña", administrador.getContraseña());
+		query.setParameter("edad", administrador.getEdad());
+		query.setParameter("cedula", administrador.getCedula());
+		query.setParameter("id", id);
+		int update = query.executeUpdate();
+		if (update > 0) {
+			return findById(id);
+		} else {
+			return Optional.empty();
 		}
-		return null;
-		
 	}
 	//Buscar administrador por filtros
-	public List<Administrador> findByFilters(String nombre, String cedula, int edad) {
-		return baseDeDatos.values().stream()
-				.filter(administrador -> nombre == null || administrador.getNombre().equalsIgnoreCase(nombre))
-				.filter(administrador -> cedula == null || administrador.getCedula().equalsIgnoreCase(cedula))
-				.filter(cliente -> edad == 0 || cliente.getEdad() == edad)
-				.filter(administrador -> administrador.getContraseña() == null || administrador.getContraseña().equalsIgnoreCase(administrador.getContraseña()))
-				.collect(Collectors.toList());
-		
+	public List<Administrador> findByFilters(String nombre) {
+		Query query = entityManager.createNativeQuery("SELECT * FROM Administrador WHERE nombre LIKE :nombre", Administrador.class);
+		query.setParameter("nombre", "%" + nombre + "%");
+		return query.getResultList();
 	}
 }
--- src/main/java/com/example/Quantum_Zone_Backend/repository/ClienteRepository.java
+++ src/main/java/com/example/Quantum_Zone_Backend/repository/ClienteRepository.java
@@ -1,54 +1,71 @@
 package com.example.Quantum_Zone_Backend.repository;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Optional;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
-
 import com.example.Quantum_Zone_Backend.modelo.Cliente;
 import java.time.LocalDate;
 @Repository
 public class ClienteRepository {
-	private final Map<String, Cliente> baseDeDatos = new HashMap<>();
+	@PersistenceContext
+	private EntityManager entityManager;
 	//Guardar cliente
+	@Transactional
 	public Cliente save(Cliente cliente) {
-		baseDeDatos.put(cliente.getId(), cliente);
+		entityManager.persist(cliente);
 		return cliente;
 	}
 	//Buscar todos los clientes
+	@Transactional
 	public List<Cliente> findAll() {
-		return new ArrayList<>(baseDeDatos.values());
+		Query query = entityManager.createNativeQuery("SELECT * FROM Cliente ", Cliente.class);
+		return query.getResultList();
 	}
 	//Buscar cliente por id
-	public Cliente findById(String id) {
-		return baseDeDatos.get(id);
+	@Transactional
+	public Optional<Cliente> findById(String id) {
+		Query query = entityManager.createNativeQuery("SELECT * FROM Cliente WHERE id = :id", Cliente.class);
+		query.setParameter("id", id);
+		try {
+			Cliente cliente = (Cliente) query.getSingleResult();
+			return Optional.of(cliente);
+		} catch (Exception e) {
+			return Optional.empty();
+		}
 	}
 	//Eliminar cliente por id
-	public void deleteById(String id) {
-		baseDeDatos.remove(id);
+	public boolean deleteById(String id) {
+		Query query = entityManager.createNativeQuery("DELETE FROM Cliente WHERE id = :id");
+		query.setParameter("id", id);
+		int delete = query.executeUpdate();
+		return delete > 0;
 	}
 	//Actualizar cliente
-	public Cliente update(Cliente cliente) {
-		if (baseDeDatos.containsKey(cliente.getId())) {
-			baseDeDatos.put(cliente.getId(), cliente);
-			return cliente;
+	public Optional<Cliente>update(String id,Cliente cliente) {
+		Query query = entityManager.createNativeQuery("UPDATE Cliente SET nombre = :nombre, edad = :edad, direccion = :direccion, imagen = :imagen, cedula = :cedula, telefono = :telefono, fechaRegistro = :fechaRegistro, email = :email WHERE id = :id");
+		query.setParameter("nombre", cliente.getNombre());
+		query.setParameter("edad", cliente.getEdad());
+		query.setParameter("direccion", cliente.getDireccion());
+		query.setParameter("imagen", cliente.getImagen());
+		query.setParameter("cedula", cliente.getCedula());
+		query.setParameter("telefono", cliente.getTelefono());
+		query.setParameter("fechaRegistro", cliente.getFechaRegistro());
+		query.setParameter("email", cliente.getEmail());
+		query.setParameter("id", cliente.getId());
+		int update = query.executeUpdate();
+		if (update > 0) {
+			return findById(id);
+		} else {
+			return Optional.empty();
 		}
-		return null;
 	}
 	//Buscar cliente por filtros
-	public List<Cliente> findByFilters(String nombre, int edad, String direccion, String imagen,String cedula, String telefono, LocalDate fechaRegistro, String email) {
-		return baseDeDatos.values().stream()
-				.filter(cliente -> nombre == null || cliente.getNombre().equalsIgnoreCase(nombre))
-				.filter(cliente -> edad == 0 || cliente.getEdad() == edad)
-				.filter(cliente -> direccion == null || cliente.getDireccion().equalsIgnoreCase(direccion))
-				.filter(cliente -> imagen == null || cliente.getImagen().equalsIgnoreCase(imagen))
-				.filter(cliente -> cedula == null || cliente.getCedula().equalsIgnoreCase(cedula))
-				.filter(cliente -> telefono == null || cliente.getTelefono().equalsIgnoreCase(telefono))
-				.filter(cliente -> fechaRegistro == null || cliente.getFechaRegistro().equals(fechaRegistro))
-				.filter(cliente -> email == null || cliente.getEmail().equalsIgnoreCase(email))
-				.collect(Collectors.toList());
-		
+	public List<Cliente> findByFilters(String nombre) {
+		Query query = entityManager.createNativeQuery("SELECT * FROM Cliente WHERE nombre LIKE :nombre", Cliente.class);
+		query.setParameter("nombre", "%" + nombre + "%");
+		return query.getResultList();
 	}
 }
